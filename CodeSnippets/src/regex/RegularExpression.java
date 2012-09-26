03051cd1e3336adcc64004c9a77aa4947b2125aa
atan <arisromil@yahoo.com>
2012-09-26 13:51:15 -0600

    Export Control Classification Number (ECCN) RegEx

--- CodeSnippets/src/regex/RegularExpression.java
+++ CodeSnippets/src/regex/RegularExpression.java
@@ -2,4 +2,7 @@ package regex;
 
 public class RegularExpression {
 
+	// All ECCNs will have 5 characters, for example, 1A001, 4B994, or 8D001
+	// http://www.bis.doc.gov/licensing/do_i_needaneccn.html
+	public static final String ECCN_PATTERN_STR = "^[0-9]{1}[a-zA-Z]{1}[0-9]{3}$";
 }
